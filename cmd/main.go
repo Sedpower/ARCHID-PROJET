670dab845556ab2b74d17374aa195400b7accc3f
Benoit-Catta <csupremenight@gmail.com>
2022-11-22 22:21:50 +0100

    enfin un code qui marche essayer pas de faire un internal ca marche pas en tout cas j'ai pas resussi

--- cmd/main.go
+++ cmd/main.go
@@ -2,40 +2,36 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"sync"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
-	opts := mqtt.NewClientOptions()
-	// AddBroker adds a broker URI to the list of brokers to be used.
-	// The format should be "scheme://host:port"
-	opts.AddBroker(brokerURI)
-	// opts.SetUsername(user)
-	// opts.SetPassword(password)
-	opts.SetClientID(clientId)
-	return opts
-}
-
-func connect(brokerURI string, clientId string) mqtt.Client {
-	fmt.Println("Trying to connect (" + brokerURI + ", " + clientId + ")...")
-	opts := createClientOptions(brokerURI, clientId)
-	client := mqtt.NewClient(opts)
-	token := client.Connect()
-	for !token.WaitTimeout(3 * time.Second) {
-	}
-	if err := token.Error(); err != nil {
-		log.Fatal(err)
-	}
-	return client
-}
-
 func main() {
 
-	client := connect("tcp://localhost:1883", "my-client-id")
+	topic := "topic/a"
 
-	client.Publish("topic/a", 0, true, "msga")
+	client := connect("tcp://localhost:1883", "my-client-id") // qualité service 2.
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		client.Subscribe(topic, 0, func(client mqtt.Client, message mqtt.Message) {
+			fmt.Printf("%s", message.Payload())
+			fmt.Println("")
+		})
+	}()
+	go func() {
+		time.Sleep(5000)
+		for i := 0; i < 10; i++ {
+			str := fmt.Sprintf("%d%s", i, "email")
+			client.Publish(topic, 0, false, str)
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+
+	fmt.Println("fini")
 
 }
