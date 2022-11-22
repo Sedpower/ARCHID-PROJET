71689b436c589586e62b389ecf8f7bc9aa9fc608
Benoit-Catta <csupremenight@gmail.com>
2022-11-22 16:32:47 +0100

    changement important base

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func createClientOptions(brokerURI string, clientId string) *mqtt.ClientOptions {
+	opts := mqtt.NewClientOptions()
+	// AddBroker adds a broker URI to the list of brokers to be used.
+	// The format should be "scheme://host:port"
+	opts.AddBroker(brokerURI)
+	// opts.SetUsername(user)
+	// opts.SetPassword(password)
+	opts.SetClientID(clientId)
+	return opts
+}
+
+func connect(brokerURI string, clientId string) mqtt.Client {
+	fmt.Println("Trying to connect (" + brokerURI + ", " + clientId + ")...")
+	opts := createClientOptions(brokerURI, clientId)
+	client := mqtt.NewClient(opts)
+	token := client.Connect()
+	for !token.WaitTimeout(3 * time.Second) {
+	}
+	if err := token.Error(); err != nil {
+		log.Fatal(err)
+	}
+	return client
+}
 
 func main() {
 
-	fmt.Println("hello")
+	client := connect("tcp://localhost:1883", "my-client-id")
 
-	fmt.Println(" jouons à pierre feuille ciseaux ")
+	// client.Subscribe("topic/a", 0, func(client mqtt.Client, message mqtt.Message) {
+	// 	fmt.Println("recu : %s", message)
+	// })
 
-	var input string
-	fmt.Scan(&input)
-
-	fmt.Printf(" vous avez joué %s ... PERDU", input)
+	client.Publish("topic/a", 0, true, "msga")
 
 }
