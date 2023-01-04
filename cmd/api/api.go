f007b0a40b7d8e3a921eee18e784625c3ca23685
Benoit-Catta <csupremenight@gmail.com>
2023-01-04 12:02:25 +0100

    swagger dans le .yaml début api.

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	redis "github.com/gomodule/redigo/redis"
 	"github.com/gorilla/mux"
-	httpSwagger "github.com/swaggo/http-swagger"
 	_ "github.com/swaggo/http-swagger/example/gorilla/docs"
 	"log"
 	"net/http"
 )
+
+// Connexion à la base de données Redis
+var pool *redis.Pool
 
 // @title Aeroport API
 // @version 1.0
@@ -20,27 +24,82 @@ import (
 // @host localhost:8080
 // @BasePath /
 func main() {
+
+	pool = &redis.Pool{
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", "localhost:6379")
+		},
+	}
+
 	router := mux.NewRouter()
 
-	// Read
-	router.HandleFunc("/api/{category}", Home).Methods("GET")
+	router.HandleFunc("/api/aeroports", Aeroports).Methods("GET")
 
-	// Swagger
-	router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
+	router.HandleFunc("/api/allMesure/{iata}/{mesure}/{annee}/{mois}/{jour}", AllMesure).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-// Home godoc
-// @Summary home get
+// liste mesure TYPE entre deux bornes DATE+HEURE / DATE+HEURE
+
+// mesure des trois types pour une DATE d'un AEROPORT
+
+// AllMesure godoc
+// @Summary retourne la moyenne des mesures sur une journée d'un aeroport
+// @Param aeroport path string true "aeroport Name"
+// @Param date path string true "Date AAAA-MM-JJ"
 // @Description get basic
 // @Success 200
-// @Router /api/{category} [get]
-func Home(w http.ResponseWriter, r *http.Request) {
-	//conn, _ := redis.Dial("tcp", "localhost:6379")
-	//_, _ := conn.Do("SET", "A", "ONE")
-	//_, _ = conn.Do("GET", "A")
+// @Router /api/mesure/{aeroport}/{date} [get]
+func AllMesure(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Printf("ici\n")
+
 	vars := mux.Vars(r)
-	w.WriteHeader(http.StatusOK)
-	fmt.Println("reçu")
-	fmt.Fprintf(w, "Category: %v\n", vars["category"])
+	iata := vars["iata"]
+	mesure := vars["mesure"]
+	annee := vars["annee"]
+	mois := vars["mois"]
+	jour := vars["jour"]
+
+	// Construction de la clé de stockage des données du jour
+	key := fmt.Sprintf("%s/%s/%s/%s/%s", iata, mesure, annee, mois, jour)
+
+	// Récupération des données du jour à partir de la clé de stockage
+	conn := pool.Get()
+	defer conn.Close()
+	data, err := redis.StringMap(conn.Do("HVALS", key))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Envoi des données au client au format JSON
+	json.NewEncoder(w).Encode(data)
+
+}
+
+type Airport struct {
+	Name string `json:"name"`
+}
+
+// Aeroports tous les aeroport
+// @Summary récupérer tous les aeroports
+// @Description retourne au format JSON tous les aeroports
+// @Success 200
+// @Router /api/aeroports [get]
+func Aeroports(w http.ResponseWriter, r *http.Request) {
+	conn := pool.Get()
+	defer conn.Close()
+	_, _ = conn.Do("HSET", "/NTE/temperature/2000/01/02", "/00/00/10", "55 2")
+	_, _ = conn.Do("HSET", "/NTE/temperature/2000/01/02", "/00/00/20", "55 3")
+	ALL_Airport := []string{"NTE", "MAD", "CDG"}
+
+	airports := []Airport{}
+
+	for _, element := range ALL_Airport {
+		airports = append(airports, Airport{Name: element})
+	}
+
+	json.NewEncoder(w).Encode(airports)
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.19
 require github.com/eclipse/paho.mqtt.golang v1.4.2 // direct
 
 require (
+	github.com/gomodule/redigo v1.8.9
 	github.com/gorilla/mux v1.8.0
-	github.com/swaggo/http-swagger v1.3.3
 	github.com/swaggo/http-swagger/example/gorilla v0.0.0-20220809182543-c8d62bfd8fdb
+	github.com/swaggo/swag v1.8.9
 	gopkg.in/yaml.v2 v2.4.0
 )
 
@@ -20,11 +21,9 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/swaggo/files v0.0.0-20220728132757-551d4a08d97a // indirect
-	github.com/swaggo/swag v1.8.8 // indirect
-	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/net v0.4.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	golang.org/x/tools v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
