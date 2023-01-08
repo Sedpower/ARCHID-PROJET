155d3129c7a401e09dbb986b9229b10bdca21968
Sedpower <erwan.chauvel@imt-atlantique.net>
2023-01-08 17:05:42 +0100

    Updated build_demo.bat

--- build_demo.bat
+++ build_demo.bat
@@ -3,5 +3,7 @@ go build -o build/sub_csv.exe ./cmd/sub/sub_csv.go
 go build -o build/pub.exe ./cmd/pub/pub.go
 go build -o build/api.exe ./cmd/api/api.go
 
+mkdir .\build\Donnees\
+
 COPY .\config.yaml .\build\config.yaml
 xcopy /E .\cmd\api\ .\build\ /EXCLUDE:exclude.txt
